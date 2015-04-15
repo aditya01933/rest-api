ef706bf4e3b38ba2bb8eaaba26153de05f269650
aditya01933 <adityatiwari01933@gmail.com>
2015-04-16 00:26:25 +0530

    fs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < Api::V1::BaseController
   private
 
     def user_params
-      params.require(:user).permit(:name, :age, :city, :state)
+      params.permit(:name, :age, :city, :state)
     end
 
     def query_params
